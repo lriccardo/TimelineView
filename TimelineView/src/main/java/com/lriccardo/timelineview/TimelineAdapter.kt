b720038cc2f09ca297319c509317fc3292663876
Riccardo Lattarulo <riccardo.lattarulo@gmail.com>
2021-12-09 18:47:07 +0100

    Add getIndicatorStyle to TimelineAdapter

--- TimelineView/src/main/java/com/lriccardo/timelineview/TimelineAdapter.kt
+++ TimelineView/src/main/java/com/lriccardo/timelineview/TimelineAdapter.kt
@@ -4,6 +4,7 @@ import androidx.annotation.ColorInt
 
 interface TimelineAdapter {
     fun getTimelineViewType(position: Int): TimelineView.ViewType? = null
+    fun getIndicatorStyle(position: Int): TimelineView.IndicatorStyle? = null
     fun getIndicatorColor(position: Int): Int? = null
     fun getLineColor(position: Int): Int? = null
 }
