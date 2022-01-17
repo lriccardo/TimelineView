baa948ed39a327acd7c13c951b8834f8913cad19
Riccardo Lattarulo <riccardo.lattarulo@gmail.com>
2022-01-17 13:05:34 +0100

    Draw TimelineDecorator inside the RecyclerView paddings

--- TimelineView/src/main/java/com/lriccardo/timelineview/TimelineDecorator.kt
+++ TimelineView/src/main/java/com/lriccardo/timelineview/TimelineDecorator.kt
@@ -57,6 +57,9 @@ class TimelineDecorator(
     }
 
     override fun onDraw(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        c.save()
+        c.clipRect(parent.paddingLeft, parent.paddingTop, parent.width, parent.height-parent.paddingBottom)
+
         parent.children.forEach {
             val itemPosition = parent.getChildAdapterPosition(it)
 
@@ -96,6 +99,7 @@ class TimelineDecorator(
                     timelineView.linePadding = it
                 }
             }
+
             timelineView.indicatorSize = indicatorSize
 
             timelineView.indicatorYPosition = indicatorYPosition
@@ -128,16 +132,17 @@ class TimelineDecorator(
             c.save()
             when (position) {
                 Position.Left -> {
-                    c.translate(padding, it.top.toFloat())
+                    c.translate(padding+parent.paddingLeft, it.top.toFloat())
                     timelineView.layout(0, 0, timelineView.measuredWidth, it.measuredHeight)
                 }
                 Position.Right -> {
-                    c.translate(it.measuredWidth + padding, it.top.toFloat())
+                    c.translate(it.measuredWidth + padding + parent.paddingLeft, it.top.toFloat())
                     timelineView.layout(0, 0, timelineView.measuredWidth, it.measuredHeight)
                 }
             }
             timelineView.draw(c)
             c.restore()
         }
+        c.restore()
     }
 }
